75ed15a6f120720eb7da1ad6a73f1b029973d7b8
esun43 <yoensun12@naver.com>
2020-06-03 22:27:52 +0900

    20200603
    add Q10808

---
+++ algorithm/baekjoon/ExArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S;
+            int[] arr = new int[26];
+            S = Console.ReadLine();
+            foreach (char c in S)
+            {
+                arr[c - 'a'] += 1;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
